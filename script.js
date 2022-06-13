cfe426ec3fb1066847f6189fe75e845b769651bd
sinsect <sin@odin.proj>
2022-06-13 16:00:47 -0400

    add playRound function to play 1 round

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function computerPlay() {
     let roll = Math.floor(Math.random() * 3);
-    console.log(roll);
     switch (roll) {
         case 0: 
             return "rock";
@@ -15,4 +14,57 @@ function computerPlay() {
             return "computer forgot to make a move!";
     }
 }
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.trim().toLowerCase();
+    console.log("Computer: " + computerSelection);
+    console.log("Player: " + playerSelection);
+    if (playerSelection == "rock") {
+        switch (computerSelection) {
+            case "rock":
+                return "Tie!";
+                break;
+            case "paper": 
+                return "You lose! Paper covers rock!";
+                break;
+            case "scissors":
+                return "You win! Rock beats scissors!";
+                break;
+            default: 
+                return;
+        }
+    }
+    else if (playerSelection == "paper") {
+        switch (computerSelection) {
+            case "rock":
+                return "You win! Paper covers rock!";
+                break;
+            case "paper": 
+                return "Tie!";
+                break;
+            case "scissors":
+                return "You lose! Scissors cut paper!";
+                break;
+            default: 
+                return;
+        }
+    }
+    else if (playerSelection == "scissors") {
+        switch (computerSelection) {
+            case "rock":
+                return "You lose! Rock beats scissors!";
+                break;
+            case "paper": 
+                return "You win! Scissors cut paper!";
+                break;
+            case "scissors":
+                return "Tie!";
+                break;
+            default: 
+                return;
+        }
+    }
+    else {
+        return "Invalid input";
+    }
+}
+console.log(playRound("   RoCK   ", computerPlay()));
